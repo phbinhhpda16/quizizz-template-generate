42a315a64b1c7030afd8c2397e684938650eb470
phbinhhpda16 <97727700+phbinhhpda16@users.noreply.github.com>
2023-05-17 22:43:32 +0700

    Update run_quizizz.bat

--- run_quizizz.bat
+++ run_quizizz.bat
@@ -1,6 +1,6 @@
 @echo OFF
 
-call C:\Users\phbin\anaconda3\Scripts\activate.bat C:\Users\phbin\anaconda3
+call C:\...\anaconda3\Scripts\activate.bat C:\...\anaconda3
 
 python quizizz-template-generate.py
 
